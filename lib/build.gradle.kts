5d044bbec2b591bdb9dccb97ed9d28727f75ecd7
Aurimas Liutikas <aurimas@google.com>
2025-05-15 12:40:31 -0700

    Add BCV

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     id("java-library")
     id("BuildPlugin")
+    id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.17.0"
 }
 
 dependencies {
